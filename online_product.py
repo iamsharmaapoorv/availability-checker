86ba9995ecf3d251b6d98bb1066299748139e439
Apoorv Sharma <iamsharmaapoorv@gmail.com>
2021-06-21 01:35:21 +0530

    Added online_product.py

---
+++ online_product.py
@@ -0,0 +1,18 @@
+import requests
+from product import Product
+from headers import DEFAULT_HEADERS
+from bs4 import BeautifulSoup
+
+
+class OnlineProduct(Product):
+    def __init__(self, url=None, in_stock_text=None):
+        self.url = url  # call super?
+        self.in_stock_text = in_stock_text
+
+    def _parse_page(self):
+        # with open('headers.json') as header_file:
+        # headers = header_file.json()
+
+        get_request = requests.get(self.url, headers=DEFAULT_HEADERS)
+        soup = BeautifulSoup(get_request.text, "html.parser")
+        return soup
