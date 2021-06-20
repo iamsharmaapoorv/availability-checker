09faf47f674f9235a35cdd2a0f5d1b4e63e9872c
Apoorv Sharma <iamsharmaapoorv@gmail.com>
2021-06-21 01:40:27 +0530

    Added notification.py

---
+++ notification.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractclassmethod
+from client import Client
+
+
+class Notification(ABC):
+
+    @staticmethod
+    @abstractclassmethod
+    def notify(title: str, content: str, client: Client) -> None:
+        pass
+
+    @staticmethod
+    def get_content(title: str, url: str) -> str:
+        content = f"{title} is available right now at {url}."
+        return content
