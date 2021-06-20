7900c8c960d7b2cd384d74411061af42dbfc84a9
Apoorv Sharma <iamsharmaapoorv@gmail.com>
2021-06-21 01:41:33 +0530

    Added email notification.py

---
+++ email_notification.py
@@ -0,0 +1,42 @@
+import smtplib
+import configparser
+from email.message import EmailMessage
+from notification import Notification
+from client import Client
+
+
+class EmailNotification(Notification):
+
+    @staticmethod
+    def notify(title: str, content: str, client: Client):
+        msg = EmailMessage()
+        msg.set_content(content)
+
+        msg['Subject'] = f'{title} is available'
+        msg['From'] = "Availability-checker@do-not-reply"
+        msg['To'] = client.email
+
+        print(content)
+        config = configparser.ConfigParser()
+        config.read('email_credentials.ini')
+        email_username = config['default']['username']
+        email_password = config['default']['password']
+        
+        # server = smtplib.SMTP('smtp.gmail.com', 587)
+        # server = smtplib.SMTP('localhost', 8025)
+        # con = server
+        # con.ehlo()
+        # con.starttls()
+        # con.ehlo()
+        # server.login(USERNAME, PASSWORD)
+        # server.sendmail(USERNAME, client.email, content)
+        # con.sendmail(email_from, email_to, email_body)
+        # con.quit()
+
+        # server.ehlo_or_helo_if_needed()
+        # server.starttls()
+        # server.ehlo_or_helo_if_needed()
+        # server.login(email_username, email_password)
+        # # server.sendmail(USERNAME, client.email, content)
+        # server.send_message(msg)
+        # server.quit()
